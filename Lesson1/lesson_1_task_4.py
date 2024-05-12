7eb0f99e919e589beb81c69e89cf64d22f9347d1
Elena Aleshina <ealeshina0@gmail.com>
2024-05-13 01:13:10 +0300

    first homework

---
+++ Lesson1/lesson_1_task_4.py
@@ -0,0 +1,3 @@
+first_name = input("Как Ваше имя? ")
+last_name = input("Как Ваша фамилия? ")
+print("Вас зовут: " + last_name +' '+ first_name)
---
+++ Lesson1/lesson_1_task_5.py
@@ -0,0 +1,3 @@
+def print_greeting(name):
+	print(name)
+print_greeting("Привет, мир!")
