89dcfcd4f5a083b986ffab5899dbd745af11318e
Elena Aleshina <ealeshina0@gmail.com>
2024-05-22 20:55:17 +0300

    add second homework

---
+++ Lesson2/lesson_2_task_3.py
@@ -0,0 +1,9 @@
+def s_square(x):
+    return(x*x)
+size = int(input("введите сторону квадрата: "))
+square  = s_square(size)
+print("площадь квадрата = ",square ) 
+
+
+
+
---
+++ Lesson2/lesson_2_task_9.py
@@ -0,0 +1,6 @@
+var_1 = 37
+var_2 = 99
+var_3 = var_1 #( =37)
+var_1 = var_2 #(= 99)
+var_2 = var_3 #(=37)
+print(var_1, var_2)
