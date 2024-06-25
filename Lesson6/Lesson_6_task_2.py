f8b432b034919445a49d021499d8359ed7320bbe
Elena Aleshina <ealeshina0@gmail.com>
2024-06-25 14:51:48 +0300

    HW6

--- Lesson6/Lesson_6_task_2.py
+++ Lesson6/Lesson_6_task_2.py
@@ -6,8 +6,8 @@ try:
     driver.get(" http://uitestingplayground.com/textinput")
     button_name = driver.find_element(
         "id", "newButtonName").send_keys("SkyPro")
-    confirm_button_name = driver.find_element("id", "updatingButton").click()
-    print(driver.find_element("id", "updatingButton").text)
+    confirm_button_name = driver.find_element("By.ID", "id_of_element").click()
+    print(driver.find_element("By.ID", "id_of_element").text)
 
 
 except Exception as ex:
